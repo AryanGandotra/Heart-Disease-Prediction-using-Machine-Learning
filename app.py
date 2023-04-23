e9715f42e67a5aaa9fa6f4e9fffe2979234fb44c
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-04-24 00:11:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 
-model = pickle.load(open('Heart-Disease-Prediction-using-Machine-Learning/blob/main/model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 st.title('Heart Disease Predictor')
 
